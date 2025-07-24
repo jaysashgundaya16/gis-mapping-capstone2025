a4b34e0e1cd1a30e516a2178ceb16b2c3d1cf57d
jaysashgundaya16 <jaysashgundaya17@gmail.com>
2025-07-24 23:29:07 +0800

    added initial files

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -3,11 +3,16 @@ import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 const MapView: React.FC = () => {
+  // Approximate bounds to cover all barangays of Manolo Fortich, Bukidnon
+  const manoloBounds: [[number, number], [number, number]] = [
+    [8.2602, 124.7740], // Southwest corner
+    [8.4602, 124.9540]  // Northeast corner
+  ];
+
   return (
-    <div style={{ height: '400px', width: '100%', borderRadius: '12px', overflow: 'hidden' }}>
+    <div style={{ height: '500px', width: '100%', borderRadius: '12px', overflow: 'hidden' }}>
       <MapContainer
-        center={[8.3602, 124.8640]} // 📍 Manolo Fortich, Bukidnon
-        zoom={13}
+        bounds={manoloBounds}
         scrollWheelZoom={true}
         style={{ height: '100%', width: '100%' }}
       >
