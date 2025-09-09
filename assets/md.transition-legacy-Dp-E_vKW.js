7f862553507df961011b04dd6d332b1cdf85b337
jaysashgundaya16 <jaysashgundaya17@gmail.com>
2025-09-09 22:36:30 +0800

    Updates

--- assets/md.transition-legacy-Dp-E_vKW.js
+++ assets/md.transition-legacy-Dp-E_vKW.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CiEXDWtf.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-B3A4cZGQ.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
