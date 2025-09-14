dfca517497665533e1ce7f85ae5a10ef1935af8d
jaysashgundaya16 <jaysashgundaya17@gmail.com>
2025-09-14 14:55:11 +0800

    Updates

--- assets/md.transition-legacy-oRBVOQmQ.js
+++ assets/md.transition-legacy-oRBVOQmQ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-B87tgGkp.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-B2GZRFfb.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
