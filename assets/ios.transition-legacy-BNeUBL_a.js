3a4f420c82aa63ef3db1313dc036ca99ffe172ae
jaysashgundaya16 <jaysashgundaya17@gmail.com>
2025-10-17 23:25:20 +0800

    Updates

--- assets/ios.transition-legacy-BNeUBL_a.js
+++ assets/ios.transition-legacy-BNeUBL_a.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-B2GZRFfb.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-B9V_trPI.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
