41637a631fa01eb4d601d02a694dc2c4757b14c2
jaysashgundaya16 <jaysashgundaya17@gmail.com>
2025-11-18 05:42:19 +0800

    Updates

--- assets/swipe-back-legacy-DNnOdx3m.js
+++ assets/swipe-back-legacy-DNnOdx3m.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Bt2YpcWb.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-BYfsAZOO.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
