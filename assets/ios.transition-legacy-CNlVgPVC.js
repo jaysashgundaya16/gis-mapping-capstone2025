5ee7f890fa9055322a1fd5d01704c74357b473b6
jaysashgundaya16 <jaysashgundaya17@gmail.com>
2025-12-19 15:34:14 +0800

    Updates

--- assets/ios.transition-legacy-CNlVgPVC.js
+++ assets/ios.transition-legacy-CNlVgPVC.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BYfsAZOO.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.q,n=t.t}],execute:function(){
+System.register(["./index-legacy-CzS0w9Aq.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.q,n=t.t}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/md.transition-legacy-BnoptwWs.js
+++ assets/md.transition-legacy-BnoptwWs.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BYfsAZOO.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.t,i=e.q}],execute:function(){
+System.register(["./index-legacy-CzS0w9Aq.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.t,i=e.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
