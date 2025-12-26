0eca38a9771b6d51ffb39be03f7b54edcf3ba09a
vevien <vevienabiera@gmail.com>
2025-12-26 10:15:47 +0800

    Fix Vite base path for vecel

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,10 @@
-/// <reference types="vitest" />
-
-import legacy from '@vitejs/plugin-legacy';
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import string from 'vite-plugin-string'; // ✅ Correct
+import react from '@vitejs/plugin-react';
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy(),
-    string({ include: ['**/*.geojson'] }) // ✅ for .geojson files
-  ],
-  base: "gis-mapping-capstone2025",
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
+  plugins: [react()],
+  base: '/',   // VERY IMPORTANT
+  build: {
+    outDir: 'dist'
   }
 });
